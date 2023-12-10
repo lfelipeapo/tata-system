4f7257181b26b4f3db0d242ecdd4056ee237ddce
Luiz Felipe Apolinário <lfelipeapo@gmail.com>
2023-12-09 23:47:38 -0300

    fix: swal alert

--- src/components/alerts/Alert.js
+++ src/components/alerts/Alert.js
@@ -1,7 +1,11 @@
 import Swal from "sweetalert2";
 
-const swalAlert = async (icon, title, text) => {
-  await Swal.fire({title: title, text: text, icon: icon});
+const swalAlert = async (title, text, icon) => {
+  await Swal.fire({
+    title: title,
+    text: text,
+    icon: icon
+  });
 };
 
 export default swalAlert;
